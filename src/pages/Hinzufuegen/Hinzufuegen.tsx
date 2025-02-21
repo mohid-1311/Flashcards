79bf937bd8bdc2489f5de4b03cd4370353d5ffe8
mohid-1311 <mohidyysyed04@gmail.com>
2025-02-21 21:16:22 +0100

    Liste erstellt, die jede Vokabel im ausgewählten Deck anzeigen soll

--- src/pages/Hinzufuegen/Hinzufuegen.tsx
+++ src/pages/Hinzufuegen/Hinzufuegen.tsx
@@ -1,6 +1,30 @@
+import { useState } from "react"
+import {getDeck, setDecks} from "../../deckState"
 function Hinzufuegen(){
+
+  const [decks, setLocalDecks] = useState(getDeck())
+
+  const [deckIndex, setDeckIndex] = useState(0)
+
+
   return(
-    <h1>Hinzufuegen</h1>
+    <div>
+      <div>
+
+      </div>
+
+      <ul>
+        {decks[deckIndex].cards.map((card: {ausdruck: string, definition: string}, index: number) => (
+          <li key={index}>
+            Ausdruck: {card.ausdruck},
+            Definition: {card.definition}
+          </li>
+        ))}
+      </ul>
+      <button>
+
+      </button>
+    </div>
   );
 }
 export default Hinzufuegen
