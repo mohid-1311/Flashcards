151dfa65c04bef0dc898feee0b36fdeb9a778444
Florian <flo.flower.76676@gmail.com>
2025-03-01 09:03:08 +0100

    Importieren und fehlendes Hinzufügen zur Startseite hinzugefügt

---
+++ src/pages/Startseite/Hinzufuegen/Hinzufuegen.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react"
+// import styles from "./JetztLernen.module.css"
+// import {Link} from "react-router-dom"
+// import {getDeck, setDecks} from "../../../deckState"
+
+function Hinzufuegen() {
+  
+  // const [localDecks, setLocalDecks] = useState(getDeck())
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+export default Hinzufuegen
--- src/pages/Startseite/Startseite.tsx
+++ src/pages/Startseite/Startseite.tsx
@@ -1,32 +1,25 @@
 import styles from "./Startseite.module.css"
 import JetztLernen from "./JetztLernen/JetztLernen"
-
-let decks = [
-  {name: "Deutsch", lastUsed: "3", cards: []},
-  {name: "Mathe", lastUsed: "5", cards: []},
-  {name: "Physik", lastUsed: "4", cards: []},
-  {name: "Englisch", lastUsed: "2", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-  {name: "Info", lastUsed: "1", cards: []},
-]
+import Hinzufuegen from "./Hinzufuegen/Hinzufuegen"
+import ImportDateifeld from "../../Components/ImportDateifeld/ImportDateifeld"
+import { useState } from "react"
+import {getDeck} from "../../deckState"
 
 function Startseite(){
+
+  const [decks, setLocalDecks] = useState(getDeck())
+
   return(
     <div className={`${styles["flex-row"]}`}>
       <div className={styles["start-element"]}>
-       <JetztLernen />
+       <JetztLernen decks={decks} />
       </div>
       <div className={`${styles["flex-column"]}`}>
         <div className={styles["start-element"]}>
-          Schnellstart Hinzufügen
+          <Hinzufuegen />
         </div>
         <div className={styles["start-element"]}>
-          Schnellstart Importieren
+          <ImportDateifeld decks={decks} setLocalDecks={setLocalDecks} />
         </div>
       </div>
     </div>
