c43c068f5f50e090c4b92ec27d4db2f0a36b937c
SimonJust1 <simon.just@sew-eurodrive.de>
2025-03-02 14:17:43 +0100

    Informationsicon hinzugefügt

--- src/pages/Lernmodi/Lernmodi.tsx
+++ src/pages/Lernmodi/Lernmodi.tsx
@@ -12,13 +12,26 @@ function Lernmodi() {
     return (
         <div className={styles.container}>
             <h2>Spielmodus auswählen</h2>
-            <button className={styles.button} onClick={() => handleNavigation('/freier-modus')}>Freier Modus</button>
-            <button className={styles.button} onClick={() => handleNavigation('/klassischer-modus')}>Klassischer Modus</button>
-            <button className={styles.button} onClick={() => handleNavigation('/schreib-modus')}>Schreib Modus</button>
+            <div className={styles['button-container']}>
+                <button className={styles.button} onClick={() => handleNavigation('/freier-modus')}>Freier Modus</button>
+                <span className={styles.infoIcon}>&#9432;
+                    <span className={styles.tooltip}>Information zum Freien Modus</span>
+                </span>
+            </div>
+            <div className={styles['button-container']}>
+                <button className={styles.button} onClick={() => handleNavigation('/klassischer-modus')}>Klassischer Modus</button>
+                <span className={styles.infoIcon}>&#9432;
+                    <span className={styles.tooltip}>Information zum Klassischen Modus</span>
+                </span>
+            </div>
+            <div className={styles['button-container']}>
+                <button className={styles.button} onClick={() => handleNavigation('/schreib-modus')}>Schreib Modus</button>
+                <span className={styles.infoIcon}>&#9432;
+                    <span className={styles.tooltip}>Information zum Schreibmodus</span>
+                </span>
+            </div>
         </div>
     );
 }
 
 export default Lernmodi;
-
-
