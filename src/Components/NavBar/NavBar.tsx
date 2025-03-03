5150c7810d8e1eba2dc05d42edb4e18e62ec7e62
Florian <flo.flower.76676@gmail.com>
2025-03-03 10:06:15 +0100

    Doppelte Zeile gelöscht, zweiter import styles

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,6 +1,5 @@
-import React from "react"
-import { Link, Navigate, useNavigate } from "react-router-dom"
-import styles from "./NavBar.module.css"
+// import React from "react"
+import { Link, useNavigate } from "react-router-dom"
 import styles from "./NavBar.module.css"
 
 function NavBar() {
