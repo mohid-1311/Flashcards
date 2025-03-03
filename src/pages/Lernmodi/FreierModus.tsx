d712a2930fceaf38e31a2759dfe72ad191fe4d48
SimonJust1 <simon.just@sew-eurodrive.de>
2025-03-03 09:48:40 +0100

    Erster Zwischenstand Freier Modus, benutzt noch css von Klassischer Modus

--- src/pages/Lernmodi/FreierModus.tsx
+++ src/pages/Lernmodi/FreierModus.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getDeck } from '../../deckState'; // Überprüfe, ob der Pfad korrekt ist
+import styles from './KlassischerModus.module.css';
+
+interface Card {
+  ausdruck: string;
+  definition: string;
+}
+
+interface Deck {
+  name: string;
+  cards: Card[];
+}
+
+//
+const deckName = "Mathe"; //Deck muss übergeben werden!
+
 
 function FreierModus() {
-    return <div>Freier Modus</div>;
+  const decks: Deck[] = getDeck();
+  const selectedDeck: Deck = decks.find((deck: Deck) => deck.name === deckName) || { name: deckName, cards: [] };
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [showDefinition, setShowDefinition] = useState<boolean>(false);
+
+  const handleNextCard = () => {
+    setShowDefinition(false);
+    if (currentIndex < selectedDeck.cards.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0); //Loop zum Anfang zurück
+    }
+  };
+
+  const handleToggleDefinition = () => {
+    setShowDefinition(!showDefinition);
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h2>Klassischer Modus - {selectedDeck.name} </h2>
+        <div className={styles.deckContainer}>
+          
+          {selectedDeck.cards.length > 0 && (
+            
+                <button onClick={handleToggleDefinition} className={styles.card}>
+                  {showDefinition
+                    ? selectedDeck.cards[currentIndex].definition
+                    : selectedDeck.cards[currentIndex].ausdruck}
+                </button>
+              
+          )}
+        </div>
+        {selectedDeck.cards.length > 0 && (
+          <div className={styles.buttonContainer}>
+            <button className={styles.button} onClick={handleNextCard}>Nächste</button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default FreierModus;
