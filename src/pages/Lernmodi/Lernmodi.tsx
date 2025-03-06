2ee2e6a18e1f369bed1d46b71be145ae17f45152
Moritz Czekalski <moritz.czekalski@sew-eurodrive.de>
2025-03-06 09:31:52 +0100

    Übergabe von DeckName zwischen Startseite und Lernmous-Auswahl

--- src/pages/Lernmodi/Lernmodi.tsx
+++ src/pages/Lernmodi/Lernmodi.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Lernmodi.module.css';
+import { useLocation } from 'react-router-dom';
 
 //Fehlt
 //Deck muss angenommen und an Lernmodus übergeben werden!
@@ -12,12 +13,16 @@ function Lernmodi() {
         navigate(path);
     };
 
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const deckName = queryParams.get('deckName');
+
     return (
         <div className={styles.container}>
             <h2>Spielmodus auswählen</h2>
             <div className={styles.buttonContainer}>
                 <div>
-                    <button className={styles.button} onClick={() => handleNavigation('/FreierModus')}>Freier Lernmodus</button>
+                    <button className={styles.button} onClick={() => handleNavigation(`/FreierModus?deckName=${deckName}`)}>Freier Lernmodus</button>
                     <span className={styles.infoIcon}>&#9432;
                         <span className={styles.tooltip}>Freier Lernmodus: <br></br>
                         Blättere einfach durch dein Deck und lerne die Karten in deinem eigenen Tempo, ohne festgelegte Abfrage oder Zeitvorgaben.
@@ -25,14 +30,14 @@ function Lernmodi() {
                     </span>
                 </div>
                 <div>
-                    <button className={styles.button} onClick={() => handleNavigation('/KlassischerModus')}>Klassischer Modus</button>
+                    <button className={styles.button} onClick={() => handleNavigation(`/KlassischerModus?deckName=${deckName}`)}>Klassischer Modus</button>
                     <span className={styles.infoIcon}>&#9432;
                         <span className={styles.tooltip}>Klassischer Lernmodus: <br></br>Lass dich abfragen, ganz klassisch.
                         </span>
                     </span>
                 </div>
                 <div>
-                    <button className={styles.button} onClick={() => handleNavigation('/SchreibModus')}>Schreib Modus</button>
+                    <button className={styles.button} onClick={() => handleNavigation(`/SchreibModus?deckName=${deckName}`)}>Schreib Modus</button>
                     <span className={styles.infoIcon}>&#9432;
                         <span className={styles.tooltip}>Schreib Lernmodus: <br></br>Teste deine Rechtschreibung.
                         </span>
