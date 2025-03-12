f2c3e9d1b5640c46a8ce32193c49cbbda663f463
Florian Blum <flo.flower.76676@gmail.com>
2025-03-12 12:16:45 +0100

    Css Selektoren in JetztLernen von Html-Elementen zu Klassen geändert.

--- src/pages/Startseite/JetztLernen/JetztLernen.tsx
+++ src/pages/Startseite/JetztLernen/JetztLernen.tsx
@@ -12,13 +12,13 @@ function JetztLernen({decks}: {decks: any}) {
 
   return (
     <div className={styles["root"]}>
-      <h1>Jetzt Lernen</h1>
+      <h1 className={styles["jetzt-lernen-titel"]}>Jetzt Lernen</h1>
       <ul className={styles["deck-liste"]}>
         {decks.length === 0
         ? <i><br />no decks available</i>
         : decks.map((deck: {name:string}, index: number)=> {
           return (
-            <Link to={`/Lernmodi?deckName=${deck.name}`}>
+            <Link className={styles["deck-liste-link"]} to={`/Lernmodi?deckName=${deck.name}`}>
               <li className={styles["deck-liste-element"]}>
                 {cutString(deck.name)}
               </li>
