c7ccfa0704805cbed2722587b6961883021c7c5e
Moritz Czekalski <moritz.czekalski@sew-eurodrive.de>
2025-05-26 09:25:56 +0200

    Gewicht zu Karteikarten in MySQL hinzugefügt hinzugefügt

--- Backend/src/db/schema/decks.ts
+++ Backend/src/db/schema/decks.ts
@@ -2,7 +2,7 @@ import { mysqlTable, AnyMySqlColumn, int, varchar } from "drizzle-orm/mysql-core
 import { benutzer } from "../schema/benutzer"
 
 export const decks = mysqlTable('decks', {
-  id: int().primaryKey(),
+  id: int().primaryKey().autoincrement(),
   name: varchar({ length: 32 }),
   benutzer_name: varchar({ length: 16 }).references((): AnyMySqlColumn => benutzer.name),
 });
--- Backend/src/db/schema/karteikarten.ts
+++ Backend/src/db/schema/karteikarten.ts
@@ -2,8 +2,9 @@ import { mysqlTable, AnyMySqlColumn, int, text } from "drizzle-orm/mysql-core"
 import { decks } from "../schema/decks"
 
 export const karteikarten = mysqlTable('karteikarten', {
-  id: int().primaryKey(),
+  id: int().primaryKey().autoincrement(),
   ausdruck: text(),
   definition: text(),
+  weight: int(),
   deck_id: int().references((): AnyMySqlColumn => decks.id)
 });
