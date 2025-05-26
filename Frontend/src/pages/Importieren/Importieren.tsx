4607d2686b8dd32dfe4b651283ede6c61bbe8cf9
Florian Blum <flo.flower.76676@gmail.com>
2025-05-26 19:26:29 +0200

    deck kann ausgewählt werden

--- Frontend/src/pages/Importieren/Importieren.tsx
+++ Frontend/src/pages/Importieren/Importieren.tsx
@@ -1,19 +1,29 @@
 import ImportDateifeld from "../../Components/ImportDateifeld/ImportDateifeld";
 import styles from "./Importieren.module.css"
 import { useState } from "react"
-// import { Deck } from "../../types"
+import { Deck } from "../../types"
 import { getDecks } from "../../deckState"
 
 function Importieren(){
 
   const [decks, setLocalDecks] = useState(getDecks())
 
-  // function cutString(str: string, maxLen: number = 30) {
-  //   if (str.length <= maxLen) {
-  //     return str
-  //   }
-  //   return str.substring(0, maxLen - 3) + "..."
-  // }
+  const [selectedDeck, setSelectedDeck] = useState("")
+
+  function cutString(str: string, maxLen: number = 30) {
+    if (str.length <= maxLen) {
+      return str
+    }
+    return str.substring(0, maxLen - 3) + "..."
+  }
+
+  function selectDeckOnClick(deckName: string) {
+    return (e: React.MouseEvent) => {
+      e.stopPropagation()
+      setSelectedDeck(deckName)
+      console.log(`export: selected ${deckName} deck`)
+    }
+  }
 
   return(
     <div className={styles["root"]}>
@@ -21,17 +31,17 @@ function Importieren(){
         <div className={styles["exportieren-header"]}>
           <h3>Exportieren</h3>
         </div>
-        {/* <ul className={styles["exportieren-liste"]}>
+        <ul className={styles["exportieren-liste"]}>
           {decks.length === 0
           ? <i><br />no decks available</i>
           : decks.map((deck: Deck, index: number)=> {
           return (
-            <li onClick={(e) => {return 0}} className={styles["exportieren-liste-element"]}>
+            <li onClick={selectDeckOnClick(deck.name)} className={styles["exportieren-liste-element"]}>
               {cutString(deck.name)}
             </li>
           )
         })}
-        </ul> */}
+        </ul>
         <h3><i>WORK IN PROGRESS</i></h3>
       </div>
       <div className={styles["importieren-div"]}>
