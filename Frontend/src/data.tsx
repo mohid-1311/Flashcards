75ed02f15c318cbe661c21c5bba82ee6abf6b8c6
SimonJust1 <simon.just@sew-eurodrive.de>
2025-07-02 14:43:40 +0200

    Data.tsx: updateCard funktion angepasst

--- Frontend/src/data.tsx
+++ Frontend/src/data.tsx
@@ -108,8 +108,33 @@ export async function getDeck(deckId: number): Promise<Omit<Deck, "cards">> {
   if(!deck) throw new Error(`Kein Deck mit der ID vorhanden!`)
 
   return deck;
-export function updateCard(): void{
+
   
+
+export async function updateCard(username: string, deckname: string, cardId: number, paramsToUpdate: Partial<Omit<Card, "id" | "deck_id">>): Promise<boolean> {
+  try {
+    const headers: Headers = new Headers();
+    headers.set("Content-Type", "application/json");
+    headers.set("Accept", "application/json");
+
+    const request = new Request(`${url}/cards/${encodeURIComponent(username)}/${encodeURIComponent(deckname)}/${cardId}`, {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(paramsToUpdate)
+    })
+
+    const response = await fetch(request);
+
+    if (!response.ok) {
+      console.error(`Fehler beim Aktualisieren der Karte: ${response.status} ${response.statusText}`);
+      return false;
+    }
+    return true
+
+  } catch (error) {
+    console.error("Fehler bei der Anfrage:", error);
+    return false;
+  }
 }
 
 //================================================================================================================
