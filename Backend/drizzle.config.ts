7857988a41fd31538041c34495f4fc1e856e6ab0
SimonJust1 <simon.just@sew-eurodrive.de>
2025-07-04 16:27:01 +0200

    merge

--- Backend/drizzle.config.ts
+++ Backend/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   out: "./drizzle/",
   schema: "./src/db/schema/*.ts",
-  dialect: "mysql",
+  dialect: "sqlite",
   dbCredentials: {
     url: process.env.DATABASE_FILE!
   }
